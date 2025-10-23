86c074c6580068044dec94e21e4f0d10c4f535aa
Alexander Koufos <exoticdft@gmail.com>
2025-10-23 20:07:15 +0000

    Replaces the explore data example with F10.7 data
    - Updates the explore data dashboard to use data from the Penticton Radio Flux dataset
    - Gets data from LATIS API
    - Plots the data

--- src/pages/explore-data/-components/DataView.tsx
+++ src/pages/explore-data/-components/DataView.tsx
@@ -1,136 +1,79 @@
-import { Alert, Box, LinearProgress, Skeleton } from '@mui/material';
-import { GridPaginationModel } from '@mui/x-data-grid';
-import React, { useState } from 'react';
-import { useFilters } from '../../../components/FilterContext';
-import { SciDataGrid } from '../../../components/SciDataGrid';
-import { filterData } from '../../../utils/filters.utils';
-import { useListQuery } from '../../../hooks/useListQuery';
-import { FilterConfig } from '../../../types/filters.types';
+import { useQuery } from '@tanstack/react-query';
+import Plot from 'react-plotly.js';
 
-interface DataViewProps {
-  filterConfigs: FilterConfig[];
-  searchTerm: string;
-  setPreviewItem: React.Dispatch<React.SetStateAction<any>>;
+async function getLaspData() {
+  const response = await fetch(
+    'https://lasp.colorado.edu/space-weather-portal/latis/dap/penticton_radio_flux.jsond'
+  );
+  const data = await response.json();
+  return data;
 }
-/**
- * Query the data rows and render as an interactive table
- */
-export const DataView: React.FC<DataViewProps> = ({
-  filterConfigs,
-  searchTerm,
-  setPreviewItem,
-}) => {
-  const { activeFilters } = useFilters();
-  const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(25);
-  const [offset, setOffest] = useState(page * pageSize);
-  // CUSTOMIZE: the unique ID field for the data source
-  const dataIdField = 'Id';
-  // CUSTOMIZE: query mode, 'client' or 'server'
-  const queryMode = 'client';
-  const { isPending, isFetching, isError, data, error } = useListQuery({
-    activeFilters,
-    // CUSTOMIZE: the table data source
-    dataSource: 'dummy-data/exoplanets.csv',
-    filterConfigs,
-    offset,
-    page,
-    pageSize,
-    queryMode,
-    staticParams: null,
+
+export function DataView() {
+  const {
+    isPending,
+    isError,
+    isSuccess,
+    data: laspData,
+    error,
+  } = useQuery({
+    queryKey: ['laspData'],
+    queryFn: getLaspData,
   });
 
-  const handleRowClick = (rowData: any) => {
-    setPreviewItem(rowData.row);
-  };
-
-  const handlePaginationModelChange = (model: GridPaginationModel) => {
-    // Reset page to first when the page size changes
-    const newPage = model.pageSize !== pageSize ? 0 : model.page;
-    const newPageSize = model.pageSize;
-    const newOffset = newPage * newPageSize;
-    setPage(newPage);
-    setPageSize(newPageSize);
-    setOffest(newOffset);
-  };
-
-  // Show a loading skeleton while the initial query is pending
   if (isPending) {
-    const emptyRows = new Array(pageSize).fill(null);
-    const indexedRows = emptyRows.map((row, i) => i);
-    return (
-      <Box
-        sx={{
-          padding: 2,
-        }}
-      >
-        {indexedRows.map((row) => (
-          <Skeleton key={row} height={50} />
-        ))}
-      </Box>
-    );
+    return <div>Loading...</div>;
   }
 
-  // Show an error message if the query fails
   if (isError) {
-    return <Alert severity="error">{error.message}</Alert>;
+    return <div>Error: {error.message}</div>;
   }
 
-  // Show the data when the query completes
-  return (
-    <>
-      {isFetching && <LinearProgress variant="indeterminate" />}
-      <SciDataGrid
-        rows={filterData(data, activeFilters, filterConfigs, searchTerm)}
-        pagination
-        paginationMode={queryMode}
-        onPaginationModelChange={handlePaginationModelChange}
-        getRowId={(row) => row[dataIdField]}
-        // CUSTOMIZE: the table columns
-        columns={[
+  if (isSuccess) {
+    const unixTime: number[] = laspData.penticton_radio_flux.data.map(
+      (i: (string | number)[]) => i[0]
+    );
+
+    const observedFlux: number[] = laspData.penticton_radio_flux.data.map(
+      (i: (string | number)[]) => i[1]
+    );
+
+    const adjustedFlux: number[] = laspData.penticton_radio_flux.data.map(
+      (i: (string | number)[]) => i[2]
+    );
+
+    return (
+      <Plot
+        data={[
           {
-            field: 'Planet Name',
-            headerName: 'Planet Name',
-            width: 200,
+            x: unixTime.map((i) => new Date(i)),
+            y: observedFlux,
+            type: 'scatter',
+            mode: 'lines',
+            name: 'Observed Flux',
           },
           {
-            field: 'Planet Host',
-            headerName: 'Planet Host',
-            width: 200,
-          },
-          {
-            field: 'Discovery Method',
-            headerName: 'Discovery Method',
-            width: 200,
-          },
-          {
-            field: 'Orbital Period Days',
-            headerName: 'Orbital Period',
-            units: 'days',
-            type: 'number',
-            width: 200,
-          },
-          {
-            field: 'Mass',
-            headerName: 'Mass',
-            units: 'Earth Mass',
-            type: 'number',
-            width: 200,
-          },
-          {
-            field: 'Eccentricity',
-            headerName: 'Eccentricity',
-            type: 'number',
-            width: 200,
+            x: unixTime.map((i) => new Date(i)),
+            y: adjustedFlux,
+            type: 'scatter',
+            mode: 'lines',
+            name: 'Adjusted Flux',
           },
         ]}
-        disableColumnSelector
-        autoHeight
-        initialState={{
-          pagination: { paginationModel: { page, pageSize } },
+        layout={{
+          title: 'Penticton Radio Flux',
+          xaxis: {
+            title: 'Time',
+          },
+          yaxis: {
+            title: 'Flux',
+          },
         }}
-        onRowClick={handleRowClick}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
       />
-    </>
-  );
-};
+    );
+  }
+}
