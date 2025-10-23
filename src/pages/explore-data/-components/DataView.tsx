eb629d3ec8f91adf97b6f51d762d0583d8ef47d7
Alexander Koufos <exoticdft@gmail.com>
2025-10-23 20:31:00 +0000

    Adds table tab to data explorer
    - Adds a tab in the explore data page to explore the data in a table

--- src/pages/explore-data/-components/DataView.tsx
+++ src/pages/explore-data/-components/DataView.tsx
@@ -1,5 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import Plot from 'react-plotly.js';
+import { useState } from 'react';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { SciDataGrid } from '../../../components/SciDataGrid';
+import { Box } from '@mui/system';
 
 async function getLaspData() {
   const response = await fetch(
@@ -10,6 +14,8 @@ async function getLaspData() {
 }
 
 export function DataView() {
+  const [view, setView] = useState('plot');
+
   const {
     isPending,
     isError,
@@ -42,38 +48,71 @@ export function DataView() {
       (i: (string | number)[]) => i[2]
     );
 
+    const tableData = unixTime.map((time, index) => ({
+      id: time,
+      time: new Date(time).toLocaleString(),
+      observedFlux: observedFlux[index],
+      adjustedFlux: adjustedFlux[index],
+    }));
+
     return (
-      <Plot
-        data={[
-          {
-            x: unixTime.map((i) => new Date(i)),
-            y: observedFlux,
-            type: 'scatter',
-            mode: 'lines',
-            name: 'Observed Flux',
-          },
-          {
-            x: unixTime.map((i) => new Date(i)),
-            y: adjustedFlux,
-            type: 'scatter',
-            mode: 'lines',
-            name: 'Adjusted Flux',
-          },
-        ]}
-        layout={{
-          title: 'Penticton Radio Flux',
-          xaxis: {
-            title: 'Time',
-          },
-          yaxis: {
-            title: 'Flux',
-          },
-        }}
-        style={{
-          width: '100%',
-          height: '100%',
-        }}
-      />
+      <Box sx={{ width: '100%', height: '100%' }}>
+        <ToggleButtonGroup
+          value={view}
+          exclusive
+          onChange={(_event, newView) => setView(newView)}
+          aria-label="data view"
+        >
+          <ToggleButton value="plot" aria-label="plot view">
+            Plot
+          </ToggleButton>
+          <ToggleButton value="table" aria-label="table view">
+            Table
+          </ToggleButton>
+        </ToggleButtonGroup>
+        {view === 'plot' ? (
+          <Plot
+            data={[
+              {
+                x: unixTime.map((i) => new Date(i)),
+                y: observedFlux,
+                type: 'scatter',
+                mode: 'lines',
+                name: 'Observed Flux',
+              },
+              {
+                x: unixTime.map((i) => new Date(i)),
+                y: adjustedFlux,
+                type: 'scatter',
+                mode: 'lines',
+                name: 'Adjusted Flux',
+              },
+            ]}
+            layout={{
+              title: 'Penticton Radio Flux',
+              xaxis: {
+                title: 'Time',
+              },
+              yaxis: {
+                title: 'Flux',
+              },
+            }}
+            style={{
+              width: '100%',
+              height: 'calc(100% - 40px)',
+            }}
+          />
+        ) : (
+          <SciDataGrid
+            rows={tableData}
+            columns={[
+              { field: 'time', headerName: 'Time', flex: 1 },
+              { field: 'observedFlux', headerName: 'Observed Flux', flex: 1 },
+              { field: 'adjustedFlux', headerName: 'Adjusted Flux', flex: 1 },
+            ]}
+          />
+        )}
+      </Box>
     );
   }
 }
